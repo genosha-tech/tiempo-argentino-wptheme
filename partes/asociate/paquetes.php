5b2100ddd6099a93c94bfda5d7d5e4f5a49b4d2d
Angie <angie@genosha.com.ar>
2021-02-10 15:23:54 -0300

    Primera pantalla de Asociate

---
+++ partes/asociate/paquetes.php
@@ -0,0 +1,227 @@
+<div class="container ta-context asociate gray-border mt-2 my-lg-5">
+    <div class="line-height-0">
+        <div class="separator m-0"></div>
+    </div>
+    <div class="subs-container">
+        <div class="section-title p-2">
+            <h4>asociate<span class="ml-2">ELEGÍ TU PAQUETE</span></h4>
+        </div>
+        <div class="container">
+            <div class="container-with-header">
+                <div class="py-2">
+                    <div class="subs-opt mt-3">
+                        <div class="title text-center">
+                            <h4>Nombre estas son las mejores opciones para asociarte</h4>
+                        </div>
+                        <div class="opt-list mt-3">
+                            <div class="d-flex flex-column flex-lg-row justify-content-center">
+                                <div class="col-12 col-lg-6 p-0 pr-lg-3">
+                                    <div class="opt digital py-3">
+                                        <div class="container">
+                                            <div class="icon d-flex justify-content-center">
+                                                <img src="../../assets/images/digital-icon.svg" alt="">
+                                            </div>
+                                            <div class="content">
+                                                <div class="container p-0">
+                                                    <div class="description text-center">
+                                                        <h4 class="m-0">Socio DIGITAL</h4>
+                                                        <p>Elegí el monto de tu abono mensual</p>
+                                                    </div>
+                                                    <div class="amounts d-flex flex-wrap">
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalAmount">
+                                                                    <p>$ 250</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalAmount">
+                                                                    <p>$ 350</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalAmount">
+                                                                    <p>$ 500</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-12 p-1">
+                                                            <div class="amount other">
+                                                                <button class="digitalAmount">
+                                                                    <p>abonar más</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="btns-container">
+                                                        <div
+                                                            class="d-flex justify-content-center mx-auto uppercase mt-3">
+                                                            <button id="digitalAmount"
+                                                                class="continue-btn">continuar</button>
+                                                        </div>
+                                                    </div>
+                                                    <div class="opt-details mt-3">
+                                                        <button type="button" data-toggle="collapse"
+                                                            data-target="#benefitsDigital" aria-expanded="false"
+                                                            aria-controls="benefitsDigital">
+                                                            <div class="d-flex">
+                                                                <div class="dropdown-icon mr-2">
+                                                                    <img src="../../assets/images/arrow.svg" alt="" />
+                                                                </div>
+                                                                <div>
+                                                                    <p>¿Qué trae este paquete?</p>
+                                                                </div>
+                                                            </div>
+                                                        </button>
+                                                        <div class="collapse" id="benefitsDigital">
+                                                            <div class="card-body">
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Beneficios exclusivos todas las semanas</h6>
+                                                                </div>
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Diario de cada domingo en versión digital</h6>
+                                                                </div>
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Adelantos a nuestros contenidos</h6>
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6 p-0 pl-lg-3">
+                                    <div class="opt digital-papel py-3">
+                                        <div class="container">
+                                            <div class="icon d-flex justify-content-center">
+                                                <img src="../../assets/images/digital-papel-icon.svg" alt="">
+                                            </div>
+                                            <div class="content">
+                                                <div class="container p-0">
+                                                    <div class="description text-center">
+                                                        <h4 class="m-0">Socio DIGITAL + PAPEL</h4>
+                                                        <p>Elegí el monto de tu abono mensual</p>
+                                                    </div>
+                                                    <div class="amounts d-flex flex-wrap">
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalPaperAmount">
+                                                                    <p>$ 500</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalPaperAmount">
+                                                                    <p>$ 700</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-4 p-1">
+                                                            <div class="amount">
+                                                                <button class="digitalPaperAmount">
+                                                                    <p>$ 1000</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-6 col-lg-12 p-1">
+                                                            <div class="amount  other">
+                                                                <button class="digitalPaperAmount">
+                                                                    <p>abonar más</p>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="btns-container">
+                                                        <div
+                                                            class=" d-flex justify-content-center mx-auto uppercase mt-3">
+                                                            <button id="digitalPaperAmount"
+                                                                class="continue-btn">continuar</button>
+                                                        </div>
+                                                    </div>
+                                                    <div class="opt-details mt-3">
+                                                        <button type="button" data-toggle="collapse"
+                                                            data-target="#benefitsDigitalPapel" aria-expanded="false"
+                                                            aria-controls="benefitsDigitalPapel">
+                                                            <div class="d-flex">
+                                                                <div class="dropdown-icon mr-2">
+                                                                    <img src="../../assets/images/arrow.svg" alt="" />
+                                                                </div>
+                                                                <div>
+                                                                    <p>¿Qué trae este paquete?</p>
+                                                                </div>
+                                                            </div>
+                                                        </button>
+                                                        <div class="collapse" id="benefitsDigitalPapel">
+                                                            <div class="card-body">
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Todos los beneficios del Socio DIGITAL</h6>
+                                                                </div>
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Ejemplar dominical impreso a domicilio</h6>
+                                                                </div>
+                                                                <div class="d-flex align-items-center">
+                                                                    <img src="../../assets/images/marker-vermas.svg"
+                                                                        alt="">
+                                                                    <h6>Ediciones especiales cada tres meses</h6>
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="opt papel py-3 py-lg-4">
+                                <div class="container">
+                                    <div class="content">
+                                        <div class="container d-lg-flex justify-content-center align-items-center p-0">
+                                            <div class="description text-center mt-2 mt-lg-0">
+                                                <div class="d-flex justify-content-center mr-lg-3">
+                                                    <div class="paper-icon mr-2">
+                                                        <img src="../../assets/images/paper-icon.svg" alt="">
+                                                    </div>
+                                                    <div class="d-flex align-items-center">
+                                                        <h4 class="m-0">Solo Edición Impresa</h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="btns-container">
+                                                <div
+                                                    class=" d-flex justify-content-center mx-auto uppercase mt-3 mt-lg-0">
+                                                    <button>elegir y continuar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
